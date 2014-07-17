583491d99bed74c24bc331ba3529e078ed3b33ab
Cristian Romanescu <cristian@romanescu.ro>
2014-07-17 19:53:52 +0300

    refs #2506: Configuring also apachesolr module

--- scripts/drupal_post_install.php
+++ scripts/drupal_post_install.php
@@ -8,12 +8,13 @@ osha_configure_solr();
 
 
 function osha_configure_solr() {
-	module_load_include('inc', 'search_api', 'search_api.admin');
-	// Configure Search API: submit search_api_admin_add_server form
 	$cfg = variable_get(OSHA_CONFIG_VARIABLE, NULL);
 	if(!empty($cfg)) {
 		$cfg = unserialize($cfg);
 		if(!empty($cfg['solr_server'])) {
+
+			// Configure Search API: submit search_api_admin_add_server form
+			module_load_include('inc', 'search_api', 'search_api.admin');
 			drupal_set_message('Creating Solr server using machine name: search_server ...');
 			$cfg = array_merge(
 				array(
@@ -64,6 +65,26 @@ function osha_configure_solr() {
 				)
 			);
 			drupal_form_submit('search_api_admin_add_server', $form_state);
+
+
+			// Configure apachesolr: submit apachesolr_environment_edit_form
+			module_load_include('inc', 'apachesolr', 'apachesolr.admin');
+			drupal_set_message('Configuring Apachesolr search environment ...');
+
+			$url = sprintf('%s://%s:%s%s', $cfg['scheme'], $cfg['host'], $cfg['port'], $cfg['path']);
+			$env_id = apachesolr_default_environment();
+			$environment = apachesolr_environment_load($env_id);
+
+			$environment['url'] = $url;
+			$environment['name'] = $cfg['name'];
+			$environment['conf']['apachesolr_direct_commit'] = $cfg['apachesolr_direct_commit'];
+			$environment['conf']['apachesolr_read_only'] = $cfg['apachesolr_read_only'];
+			$environment['conf']['apachesolr_soft_commit'] = $cfg['apachesolr_soft_commit'];
+
+			apachesolr_environment_save($environment);
+			// @todo: See ticket #2527 - cannot make the form save new settings!
+			// drupal_form_submit('apachesolr_environment_edit_form', $form_state, $environment);
+
 		} else {
 			drupal_set_message('Unable to find solr_server section in config. Solr integration may be broken', 'warning');
 		}
