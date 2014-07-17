5a747b80aef344e6e3b56a0d599c510fa30d8e01
Cristian Romanescu <cristian@romanescu.ro>
2014-07-17 13:03:13 +0300

    fixes #2519 - data directory is now read from config.json

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+# Extract variables from conf/config.json and expose inside current environment
+migration_data_dir=`php scripts/config_parser.php osha_data_dir`
+ret=$?
+
+if [ ! "${ret}" -eq 0 ]; then
+	echo "Error getting environment variable osha_data_dir! Quitting ...";
+	exit ${ret}
+fi
+
 # Setup a clean site
 cd docroot/
 drush site-install -y
@@ -6,7 +16,7 @@ drush init
 drush build
 
 # Configuring migrations
-drush vset osha_data_dir /Work/osha/data
+drush vset osha_data_dir ${migration_data_dir}
 
 echo "Registering migrations ..."
 drush migrate-auto-register
