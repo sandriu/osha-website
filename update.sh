eb367bbfad7fa07ba78c92435f2f46b094aba199
Cristian Romanescu <cristian@romanescu.ro>
2014-07-29 14:54:15 +0300

    Format code in update.sh

--- update.sh
+++ update.sh
@@ -7,20 +7,20 @@ drush build
 
 if [ "$1" == "--migrate" ]; then
 
-	echo "Updating NACE codes taxonomy"
-	drush migrate-import NaceCodes --update
+  echo "Updating NACE codes taxonomy"
+  drush migrate-import NaceCodes --update
 
-	echo "Updating ESENER taxonomy"
-	drush migrate-import EsenerTaxonomy --update
+  echo "Updating ESENER taxonomy"
+  drush migrate-import EsenerTaxonomy --update
 
-	echo "Updating Publication types taxonomy"
-	drush migrate-import PublicationTypesTaxonomy --update
+  echo "Updating Publication types taxonomy"
+  drush migrate-import PublicationTypesTaxonomy --update
 
-	echo "Updating Multilingual Thesaurus taxonomy"
-	drush migrate-import ThesaurusTaxonomy --update
+  echo "Updating Multilingual Thesaurus taxonomy"
+  drush migrate-import ThesaurusTaxonomy --update
 
-	echo "Importing Categories taxonomy"
-	drush migrate-import TagsTaxonomy
+  echo "Importing Categories taxonomy"
+  drush migrate-import TagsTaxonomy
 
 fi
 
