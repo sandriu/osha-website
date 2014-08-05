5948e6eaa06fdf240a988df7d5023d2a572c99f8
Dragos Dumitrescu <dumitrescu.dragos1@gmail.com>
2014-08-05 13:15:41 +0300

    Added patch command and patch for entity_translation

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -2,7 +2,18 @@
 
 echo "Applying existing patches to the instance ..."
 
+#Declare path variables to be used in patch paths
+PATCHDIR=$PWD/patches
+DOCROOT=$PWD/docroot
+
 # Put your patch commands here ...
+
+# entity_translation
+# Fix bug of incorrect language none for pathauto alias
+# https://www.drupal.org/node/1925848
+# https://www.drupal.org/files/entitytranslation-incorrect_pathauto_pattern-1925848-8.patch
+patch -d $DOCROOT/sites/all/modules/contrib/entity_translation -N -r /dev/null -p1 < $PATCHDIR/entity_translation/entitytranslation-incorrect_pathauto_pattern-1925848-8.patch
+
 
 
 # Clear the cache to make sure existing instace see the applied patches
