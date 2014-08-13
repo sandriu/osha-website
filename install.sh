6e42ef43ed173bcfa992e8a6ef989f2e035199ac
Razvan Tache <razvan.tache@eaudeweb.ro>
2014-08-13 17:50:51 +0300

    refs #2645: Include taxonomy 'Activity' migration in 'install.sh' and in 'update.sh' scripts

--- install.sh
+++ install.sh
@@ -21,6 +21,9 @@ drush migrate-auto-register
 
 if [ "$1" == "--migrate" ]; then
 
+	echo "Importing Activity taxonomy"
+	drush migrate-import TaxonomyActivity
+
 	echo "Importing NACE codes taxonomy"
 	drush migrate-import TaxonomyNaceCodes
 
