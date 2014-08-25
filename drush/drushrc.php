9e4a09c94b4c590264867462bca7be7e5197eb16
Cristian Romanescu <cristian@romanescu.ro>
2014-08-25 20:38:53 +0300

    refs #2705 - Remove two unused sources for translations (locale and node)

--- drush/drushrc.php
+++ drush/drushrc.php
@@ -304,9 +304,7 @@ $options['init-modules'] = array(
   'tmgmt_entity_ui',
   'tmgmt_ui',
   'tmgmt_file',
-  'tmgmt_node',
   'tmgmt_i18n_string',
-  'tmgmt_locale',
 
   'i18n_menu',
   'i18n_block',
