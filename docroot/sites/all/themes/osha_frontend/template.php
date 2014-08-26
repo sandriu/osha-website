3c907e631d0ebf0c399005c15aff3ff55abc3d24
Dragos Dumitrescu <dumitrescu.dragos1@gmail.com>
2014-08-26 11:57:52 +0300

    Date formatter now supports calendar theme refs #2717;

--- docroot/sites/all/themes/osha_frontend/template.php
+++ docroot/sites/all/themes/osha_frontend/template.php
@@ -1,4 +1,36 @@
 <?php
 /**
- * @file template.php
+ * @file
+ * Osha frontend template functionality
  */
+
+/**
+ * Implements hook_theme().
+ */
+function osha_frontend_date_display_single(&$variables) {
+  $date_theme = '';
+  if (!empty($variables['dates']['value']['osha_date_theme'])) {
+    $date_theme = $variables['dates']['value']['osha_date_theme'];
+  }
+  switch ($date_theme) {
+    case 'calendar':
+      return osha_frontend_date_calendar_icon($variables);
+
+    default:
+      return theme_date_display_single($variables);
+  }
+}
+
+/**
+ * Split the date into spans to be formatted as calendar icon.
+ */
+function osha_frontend_date_calendar_icon($variables) {
+  $time = strtotime($variables['date']);
+  $month = date('m', $time);
+  $day = date('d', $time);
+  return
+    '<div class="osha-date-calendar">
+      <span class="osha-date-calendar-month">' . $month . '</span>
+      <span class="osha-date-calendar-day">' . $day . '</span>
+    </div>';
+}
