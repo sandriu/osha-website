f63c66199e2fceeadd13ded00e5edba55dca70b6
Dragos Dumitrescu <dumitrescu.dragos1@gmail.com>
2014-08-27 18:48:36 +0300

    Changed frontend theme to support menu_block refs #2715

--- docroot/sites/all/themes/osha_frontend/template.php
+++ docroot/sites/all/themes/osha_frontend/template.php
@@ -2,3 +2,11 @@
 /**
  * @file template.php
  */
+
+function osha_frontend_links__system_main_menu() {
+  return NULL;
+}
+
+function osha_frontend_menu_tree__main_menu($variables) {
+  return '<ul id="main-menu-links" class="menu clearfix">'.$variables['tree'].'</ul>';
+}
