8dcc8734eaab2f5e946e8d8cc248abed9dcb0d36
Claudia Ifrim <claudia.ifrim@eaudeweb.ro>
2014-09-09 15:05:11 +0300

    add the newsletter template for frontend theme

--- docroot/sites/all/themes/osha_frontend/templates/entity_collection.tpl.php
+++ docroot/sites/all/themes/osha_frontend/templates/entity_collection.tpl.php
@@ -1,17 +1,68 @@
 <?php
-// TODO:EDW: Add e-mail template here.
-?>
-<div class="<?php print $classes; ?>">
-  <?php print render($title_prefix); ?>
-  <?php if ($show_title): ?>
-    <h2><?php print $title; ?></h2>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-  <?php foreach ($collection as $item): ?>
-    <div class="container">
-      <div class="item">
-        <?php print render($item); ?>
+if (module_exists('osha_newsletter') && isset($variables['element'])) {
+  $module_templates_path = drupal_get_path('module','osha_newsletter').'/templates';
+  if ((isset($variables['element']['#entity_type']) && $variables['element']['#entity_type'] == 'entity_collection')
+    && (isset($variables['element']['#bundle']) && $variables['element']['#bundle'] == 'newsletter_content_collection')) {
+    $source = $variables['element']['#entity_collection'];
+    $content = entity_collection_load_content('newsletter_content_collection', $source);
+    $items = $content->children;
+
+    // preprocess data
+    $elements = array();
+    $last_section = NULL;
+    $blogs = array();
+    $news = array();
+    $events = array();
+
+    foreach ($items as $item) {
+      if ($item->type == 'taxonomy_term') {
+        $term = taxonomy_term_view($item->content, 'token');
+        $last_section = $item->content->name_original;
+        if ($last_section == 'Blog') {
+          $blogs[] = $term;
+        } else if ($last_section == 'News') {
+          $news[] = $term;
+        } else if ($last_section == 'Events') {
+          $events[] = $term;
+        } else {
+          $elements[] = $term;
+        }
+      } else if ($item->type == 'node') {
+        $style = $item->style;
+        $node = node_view($item->content,$style);
+
+        if ($last_section == 'Blog') {
+          $blogs[] = $node;
+        } else if ($last_section == 'News') {
+          $news[] = $node;
+        } else if ($last_section == 'Events') {
+          $events[] = $node;
+        } else {
+          $elements[] = $node;
+        }
+      }
+    }
+
+    print theme_render_template($module_templates_path.'/newsletter_header.tpl.php', array('languages' => NULL));
+    print theme_render_template($module_templates_path.'/newsletter_body.tpl.php', array('items' => $elements, 'blogs' => $blogs, 'news' => $news, 'events' => $events));
+    print theme_render_template($module_templates_path.'/newsletter_footer.tpl.php', array());
+  }
+} else {
+  ?>
+  <div class="<?php print $classes; ?>">
+    <?php print render($title_prefix); ?>
+    <?php if ($show_title): ?>
+      <h2><?php print $title; ?></h2>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
+    <?php foreach ($collection as $item): ?>
+      <div class="container">
+        <div class="item">
+          <?php print render($item); ?>
+        </div>
       </div>
-    </div>
-  <?php endforeach; ?>
-</div>
+    <?php endforeach; ?>
+  </div>
+<?php
+}
+?>
