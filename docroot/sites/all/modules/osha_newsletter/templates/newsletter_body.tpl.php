41140b08f4b29f30f5c7ca62051e549874dd62b7
sandriu <sonia.andriu@eaudeweb.ro>
2014-09-09 16:19:12 +0300

    Refs #2567: refactor width for left and right columns of newsletter body

--- docroot/sites/all/modules/osha_newsletter/templates/newsletter_body.tpl.php
+++ docroot/sites/all/modules/osha_newsletter/templates/newsletter_body.tpl.php
@@ -1,56 +1,65 @@
-<table>
+<table border="0" cellpadding="28" cellspacing="0" width="800">
   <tbody>
     <tr>
-      <td width="590" valign="top">
-        <?php
-          foreach ($items as $item) {
-            print(render($item));
-          }
-        ?>
-      </td>
-      <td width="200" valign="top"  style="padding-left: 24px; padding-top: 36px;">
-        <div style="font: 12px Verdana,Helvetica,'Lucida Grande',Lucida,Arial,sans-serif;">
-          <?php
-            if (!empty($blogs) && sizeof($blogs) > 1) {
-              foreach ($blogs as $item) {
-                print(render($item));
-              }
-            }
-          ?>
-        </div>
-        <div style="background-color: #f2f4f6; font: 12px Verdana,Helvetica,'Lucida Grande',Lucida,Arial,sans-serif; margin-bottom: 20px;">
-          <h2 style="color: #191919; font-size: 11px; font-weight: normal; margin-bottom: 13px; padding: 6px 15px; text-align: right;">
-            <?php print l(t('View the blog'), 'https://osha.europa.eu/en/about/director_corner/blog', array('attributes' => array('style' => 'color: #144989; text-decoration: none;'), 'external' => TRUE)); ?> &raquo;
-          </h2>
-        </div>
-        <div style="font: 12px Verdana,Helvetica,'Lucida Grande',Lucida,Arial,sans-serif;">
-          <?php
-            if (!empty($news) && sizeof($news) > 1) {
-              foreach ($news as $item) {
-                print(render($item));
-              }
-            }
-          ?>
-        </div>
-        <div style="background-color: #f2f4f6; font: 12px Verdana,Helvetica,'Lucida Grande',Lucida,Arial,sans-serif; margin-bottom: 20px;">
-          <h2 style="color: #191919; font-size: 11px; font-weight: normal; margin-bottom: 13px; padding: 6px 15px; text-align: right;">
-            <?php print l(t('More news'), 'https://osha.europa.eu/en/news', array('attributes' => array('style' => 'color: #144989; text-decoration: none;'), 'external' => TRUE)); ?> &raquo;
-          </h2>
-        </div>
-        <div style="font: 12px Verdana,Helvetica,'Lucida Grande',Lucida,Arial,sans-serif;">
-          <?php
-            if (!empty($events) && sizeof($events) > 1) {
-              foreach ($events as $item) {
-                print(render($item));
-              }
-            }
-          ?>
-        </div>
-        <div style="background-color: #dce8f4; font: 12px Verdana,Helvetica,'Lucida Grande',Lucida,Arial,sans-serif; margin-bottom: 20px;">
-          <h2 style="color: #191919; font-size: 11px; font-weight: normal; margin-bottom: 13px; padding: 6px 15px; text-align: right;">
-            <?php print l(t('More events'), 'https://osha.europa.eu/en/news', array('attributes' => array('style' => 'color: #144989; text-decoration: none;'), 'external' => TRUE)); ?> &raquo;
-          </h2>
-        </div>
+      <td  width="800" style="padding-top: 0px; padding-bottom: 0px;">
+        <table border="0" cellpadding="20" cellspacing="0" width="800">
+          <tbody>
+            <tr>
+              <td width="450" style="padding-top: 0px;" class="left-column">
+                <?php
+                  foreach ($items as $item) {
+                    print(render($item));
+                  }
+                ?>
+              </td>
+
+              <td width="350" style="vertical-align: top; padding-top: 0px; padding-right: 0px;" class="right-column">
+                <div style="font: 12px Verdana,Helvetica,'Lucida Grande',Lucida,Arial,sans-serif;">
+                  <?php
+                    if (!empty($blogs) && sizeof($blogs) > 1) {
+                      foreach ($blogs as $item) {
+                        print(render($item));
+                      }
+                    }
+                  ?>
+                </div>
+                <div style="background-color: #f2f4f6; font: 12px Verdana,Helvetica,'Lucida Grande',Lucida,Arial,sans-serif; margin-bottom: 20px;">
+                  <h2 style="color: #191919; font-size: 11px; font-weight: normal; margin-bottom: 13px; padding: 6px 15px; text-align: right;">
+                    <?php print l(t('View the blog'), 'https://osha.europa.eu/en/about/director_corner/blog', array('attributes' => array('style' => 'color: #144989; text-decoration: none;'), 'external' => TRUE)); ?> &raquo;
+                  </h2>
+                </div>
+                <div style="font: 12px Verdana,Helvetica,'Lucida Grande',Lucida,Arial,sans-serif;">
+                  <?php
+                    if (!empty($news) && sizeof($news) > 1) {
+                      foreach ($news as $item) {
+                        print(render($item));
+                      }
+                    }
+                  ?>
+                </div>
+                <div style="background-color: #f2f4f6; font: 12px Verdana,Helvetica,'Lucida Grande',Lucida,Arial,sans-serif; margin-bottom: 20px;">
+                  <h2 style="color: #191919; font-size: 11px; font-weight: normal; margin-bottom: 13px; padding: 6px 15px; text-align: right;">
+                    <?php print l(t('More news'), 'https://osha.europa.eu/en/news', array('attributes' => array('style' => 'color: #144989; text-decoration: none;'), 'external' => TRUE)); ?> &raquo;
+                  </h2>
+                </div>
+                <div style="font: 12px Verdana,Helvetica,'Lucida Grande',Lucida,Arial,sans-serif;">
+                  <?php
+                    if (!empty($events) && sizeof($events) > 1) {
+                      foreach ($events as $item) {
+                        print(render($item));
+                      }
+                    }
+                  ?>
+                </div>
+                <div style="background-color: #dce8f4; font: 12px Verdana,Helvetica,'Lucida Grande',Lucida,Arial,sans-serif; margin-bottom: 20px;">
+                  <h2 style="color: #191919; font-size: 11px; font-weight: normal; margin-bottom: 13px; padding: 6px 15px; text-align: right;">
+                    <?php print l(t('More events'), 'https://osha.europa.eu/en/news', array('attributes' => array('style' => 'color: #144989; text-decoration: none;'), 'external' => TRUE)); ?> &raquo;
+                  </h2>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </td>
     </tr>
   </tbody>
