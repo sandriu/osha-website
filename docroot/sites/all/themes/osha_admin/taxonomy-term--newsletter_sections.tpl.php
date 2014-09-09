cee831cece7754c25cb5c539d68b4223cf149044
sandriu <sonia.andriu@eaudeweb.ro>
2014-09-09 16:31:08 +0300

    Refs #2567: refactor HTML and CSS for newsletter categories name

--- docroot/sites/all/themes/osha_admin/taxonomy-term--newsletter_sections.tpl.php
+++ docroot/sites/all/themes/osha_admin/taxonomy-term--newsletter_sections.tpl.php
@@ -1,11 +1,15 @@
-<div style="font: 12px Verdana,Helvetica,'Lucida Grande',Lucida,Arial,sans-serif;">
-  <h1 style="color: #191919; font: 18px/35px Verdana,Helvetica,'Lucida Grande',Lucida,Arial,sans-serif; letter-spacing: 2px; margin: 34px 0 11px; text-shadow: 2px 2px 5px #dddddd; text-transform: uppercase;">
-    <?php
-      if (isset($name_field[$language])) {
-        print($name_field[$language][0]['safe_value']);
-      } else {
-        print($name_field[0]['safe_value']);
-      }
-    ?>
-  </h1>
-</div>
+<table border="0" cellpadding="10" cellspacing="0" width="100%" class="category-name">
+  <tbody>
+    <tr>
+      <td style="font-family: Oswald, Arial, sans-serif; font-weight: 300px; font-size: 20px; padding-left: 0px; padding-right: 0px;">
+        <?php
+          if (isset($name_field[$language])) {
+            print($name_field[$language][0]['safe_value']);
+            } else {
+              print($name_field[0]['safe_value']);
+          }
+        ?>
+      </td>
+    </tr>
+  </tbody>
+</table>
