a059acfaed14f57eb8e52bc0f021b7b5e45a20a3
sandriu <sonia.andriu@eaudeweb.ro>
2014-09-10 12:23:31 +0300

    Refs #2567: styles for pink arrow from View blog link

--- docroot/sites/all/modules/osha_newsletter/templates/newsletter_body.tpl.php
+++ docroot/sites/all/modules/osha_newsletter/templates/newsletter_body.tpl.php
@@ -23,11 +23,30 @@
                     }
                   ?>
                 </div>
-                <div style="background-color: #f2f4f6; font: 12px Verdana,Helvetica,'Lucida Grande',Lucida,Arial,sans-serif; margin-bottom: 20px;">
-                  <h2 style="color: #191919; font-size: 11px; font-weight: normal; margin-bottom: 13px; padding: 6px 15px; text-align: right;">
-                    <?php print l(t('View the blog'), 'https://osha.europa.eu/en/about/director_corner/blog', array('attributes' => array('style' => 'color: #144989; text-decoration: none;'), 'external' => TRUE)); ?> &raquo;
-                  </h2>
-                </div>
+                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-top-width: 2px; border-style: dotted; border-color: #CFDDEE; padding-bottom: 15px;" class="pink-arrow">
+                  <tbody>
+                    <tr>
+                      <td style="font-family: Oswald, Arial, sans-serif; font-size: 18px; color: #003399; text-align: right;">
+                        <span>
+                          <?php print l(t('View the blog'), 'https://osha.europa.eu/en/about/director_corner/blog', array('attributes' => array('style' => 'color: #144989; text-decoration: none;'), 'external' => TRUE)); ?>
+                          <?php
+                            $directory = drupal_get_path('module','osha_newsletter');
+                            $site_url = variable_get('site_base_url', 'http://osha.localhost');
+                            print l(theme('image', array(
+                            'path' => $directory . '/images/pink-arrow.png',
+                            'width' => 19,
+                            'height' => 11,
+                            'alt' => 'link arrow',
+                            )), $site_url, array(
+                            'html' => TRUE,
+                            'external' => TRUE
+                            ));
+                          ?>
+                        </span>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
                 <div style="font: 12px Verdana,Helvetica,'Lucida Grande',Lucida,Arial,sans-serif;">
                   <?php
                     if (!empty($news) && sizeof($news) > 1) {
