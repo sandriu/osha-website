600a24d290582fd99595c60c04c2e8fcc3625a46
sandriu <sonia.andriu@eaudeweb.ro>
2014-09-16 15:50:11 +0300

    Refs #2567: add 800px wide wrapper for newsletter tables

--- docroot/sites/all/themes/osha_admin/entity_collection.tpl.php
+++ docroot/sites/all/themes/osha_admin/entity_collection.tpl.php
@@ -51,10 +51,14 @@ if (module_exists('osha_newsletter') && isset($variables['element'])) {
     usort($languages, function ($a, $b) {
       return strcmp($a->name, $b->name);
     });
-
-    print theme_render_template($module_templates_path.'/newsletter_header.tpl.php', array('languages' => $languages, 'newsletter_title' => $newsletter_title, 'newsletter_id' => $newsletter_id, 'newsletter_date' => $newsletter_date));
-    print theme_render_template($module_templates_path.'/newsletter_body.tpl.php', array('items' => $elements, 'blogs' => $blogs, 'news' => $news, 'events' => $events));
-    print theme_render_template($module_templates_path.'/newsletter_footer.tpl.php', array());
+    ?>
+    <div class="newsletter-wrapper" style="width: 800px;">
+      <?php
+        print theme_render_template($module_templates_path.'/newsletter_header.tpl.php', array('languages' => $languages, 'newsletter_title' => $newsletter_title, 'newsletter_id' => $newsletter_id, 'newsletter_date' => $newsletter_date));
+        print theme_render_template($module_templates_path.'/newsletter_body.tpl.php', array('items' => $elements, 'blogs' => $blogs, 'news' => $news, 'events' => $events));
+        print theme_render_template($module_templates_path.'/newsletter_footer.tpl.php', array());
+      ?>
+    </div><?php
   }
 } else {
 ?>
