375c0bcbdcd3029a6a3efaf462a1deed5f7d1756
Cristian Romanescu <cristian@romanescu.ro>
2014-09-24 22:45:58 +0300

    CW-384: Added placeholder in the search text field

--- docroot/sites/all/themes/osha_frontend/template.php
+++ docroot/sites/all/themes/osha_frontend/template.php
@@ -100,6 +100,15 @@ function osha_frontend_process_node(&$vars) {
 }
 
 /**
+ * Implements hook_form_alter().
+ */
+function osha_frontend_form_alter(&$form, &$form_state, $form_id) {
+  if ($form_id == 'search_block_form') {
+    $form['search_block_form']['#attributes']['placeholder'] = t('Search');
+  }
+}
+
+/**
  * @todo @Ivan: Edit only below
  */
 
