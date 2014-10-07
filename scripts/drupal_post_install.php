2acb8e976a5a6e3c00ce3e55cd902fa86bf35d30
Dragos Dumitrescu <dumitrescu.dragos1@gmail.com>
2014-10-07 20:14:33 +0300

    Changed post install function search autocomplete callback

--- scripts/drupal_post_install.php
+++ scripts/drupal_post_install.php
@@ -161,24 +161,14 @@ function osha_configure_search_autocomplete() {
     ))
     ->condition('selector', '#edit-search-block-form--2', '<>')
     ->execute();
-  // Configure the search form.
-  $fid = db_select('search_autocomplete_forms', 'f')
-    ->fields('f', array('fid'))
+  db_update('search_autocomplete_forms')
+    ->fields(array(
+      'data_view' => '',
+      'theme' => 'basic-blue.css',
+      'data_callback' => 'search/site/autocomplete',
+    ))
     ->condition('selector', '#edit-search-block-form--2')
-    ->execute()->fetchField(0);
-  if ($fid) {
-    db_update('search_autocomplete_forms')
-      ->fields(array(
-        'data_view' => 'solr_autocomplete',
-        'theme' => 'basic-blue.css',
-        'data_callback' => 'search_autocomplete/autocomplete/' . $fid . '/',
-      ))
-      ->condition('selector', '#edit-search-block-form--2')
-      ->execute();
-  }
-  else {
-    drupal_set_message('Failed to configure search_autocomplete form', 'error');
-  }
+    ->execute();
 }
 
 /**
