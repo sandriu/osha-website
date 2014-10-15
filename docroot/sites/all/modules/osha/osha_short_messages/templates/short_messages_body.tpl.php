056930e477c3c02e5daa5ec46413c0a190b0d82c
Radu Ciofringeanu <radu.sc@gmail.com>
2014-10-15 16:55:12 +0300

    Display message in an iframe

---
+++ docroot/sites/all/modules/osha/osha_short_messages/templates/short_messages_body.tpl.php
@@ -0,0 +1,9 @@
+<table border="0" cellpadding="28" cellspacing="0" width="800">
+  <tbody>
+  <tr>
+    <td style="padding-top: 15px; padding-bottom: 15px; font-family: Arial,sans-serif; font-size: 12px; color: #333333;">
+      <?php print $body_content; ?>
+    </td>
+  </tr>
+  </tbody>
+</table>
