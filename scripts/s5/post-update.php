a064724d8c98b9829c1afe15056ce1f63ea2354a
Andrei Melis <andrei.melis@eaudeweb.ro>
2014-11-06 15:09:13 +0200

    Update script for branch sprint-5

---
+++ scripts/s5/post-update.php
@@ -0,0 +1,10 @@
+<?php
+
+/* Add new stuff for deployment of sprint-4 branch here. To avoid conflicts, add them after your name 
+ * Also include the ticket number, see example below
+ */
+// andrei
+// cristi
+// dragos
+// claudia
+// radu
