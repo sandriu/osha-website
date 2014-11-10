24b19e3931628f469d9d353316a942914bb730c3
Claudia Ifrim <ifrim.claudia@gmail.com>
2014-11-10 12:06:45 +0200

    CW-601 - Home Page- View all link should be moved under highlights, ROS graphical design is not correct.

--- update-s5.sh
+++ update-s5.sh
@@ -35,6 +35,7 @@ drush features-revert --force -y osha_breadcrumbs
 drush features-revert --force -y osha_highlight
 drush features-revert --force -y osha_news
 drush features-revert --force -y osha_publication
+drush features-revert --force -y osha_highlight
 drush cc all
 
 echo "UPDATEDB ..."
