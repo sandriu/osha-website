687d44a77ac9c3b188f74f947630848351044b2c
Dragos Dumitrescu <dumitrescu.dragos1@gmail.com>
2014-11-12 14:51:11 +0200

    Fix update s4 scripts

--- scripts/s4/pre-update.php
+++ scripts/s4/pre-update.php
@@ -1,7 +1,7 @@
 <?php
 
 
-/* Add new stuff for deployment of sprint-4 branch here. To avoid conflicts, add them after your name 
+/* Add new stuff for deployment of sprint-4 branch here. To avoid conflicts, add them after your name
  * Also include the ticket number, see example below
  */
 // andrei
@@ -18,5 +18,25 @@ field_delete_field('field_needs_retranslation');
 
 // cristi
 // dragos
+drush_log('Dropping field: field_pr_related_links', 'ok');
+field_delete_field('field_pr_related_links');
+
+drush_log('Dropping field: field_pr_release_date', 'ok');
+field_delete_field('field_pr_release_date');
+if (module_exists('aggregator') && module_load_include('inc', 'aggregator', 'aggregator.admin')) {
+  drupal_set_message('Add press releases rss feed ...');
+
+  $form_state = array(
+    'values' => array(
+      'title' => 'EU-OSHA in the media',
+      'url' => 'http://portal.kantarmedia.de/rss/index/1002043/100000063/1024803/9a7b629357e748080ff47e4d0db7ec57cffff3fe',
+      'refresh' => 900,
+      'block' => 2,
+      'op' => 'Save',
+    ),
+  );
+
+  drupal_form_submit('aggregator_form_feed', $form_state);
+}
 // claudia
 // radu
--- update_s4_before.sh
+++ update_s4_before.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Disabling the modules no longer in use ..."
-drush dis -y osha_taxonomies_uuid osha_content osha_content edw
+drush dis -y osha_taxonomies_uuid osha_content osha_content_edw
 
 echo "PRE-UPDATE tasks ..."
 drush php-script ../scripts/s4/pre-update.php
