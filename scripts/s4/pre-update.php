0b1993e874cd6c88d21529693c3576f5c30c4e9c
Andrei Melis <andrei.melis@eaudeweb.ro>
2014-11-13 15:40:44 +0200

    Removed submit aggregator_form_feed, already included in module update

--- scripts/s4/pre-update.php
+++ scripts/s4/pre-update.php
@@ -23,20 +23,6 @@ field_delete_field('field_pr_related_links');
 
 drush_log('Dropping field: field_pr_release_date', 'ok');
 field_delete_field('field_pr_release_date');
-if (module_exists('aggregator') && module_load_include('inc', 'aggregator', 'aggregator.admin')) {
-  drupal_set_message('Add press releases rss feed ...');
 
-  $form_state = array(
-    'values' => array(
-      'title' => 'EU-OSHA in the media',
-      'url' => 'http://portal.kantarmedia.de/rss/index/1002043/100000063/1024803/9a7b629357e748080ff47e4d0db7ec57cffff3fe',
-      'refresh' => 900,
-      'block' => 2,
-      'op' => 'Save',
-    ),
-  );
-
-  drupal_form_submit('aggregator_form_feed', $form_state);
-}
 // claudia
 // radu
