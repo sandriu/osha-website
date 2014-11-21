0302314de2ba51cb57663bcd429d8e9c6bbf3b99
Andrei Melis <andrei.melis@eaudeweb.ro>
2014-11-21 15:24:54 +0200

    Back class for blog link

--- docroot/sites/all/themes/osha_frontend/templates/node--blog.tpl.php
+++ docroot/sites/all/themes/osha_frontend/templates/node--blog.tpl.php
@@ -1,6 +1,6 @@
 <?php if($page): ?>
   <h1 id="page-title" class="page__title title">&nbsp;</h1>
-  <div class="view-header"><?php print l(t('Back to latest posts'), 'articles/blog'); ?></div>
+  <div class="view-header back"><?php print l(t('Back to latest posts'), 'articles/blog'); ?></div>
 <?php endif; ?>
 
 <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
