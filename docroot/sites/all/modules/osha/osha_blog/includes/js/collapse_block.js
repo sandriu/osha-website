a6a25a0271144a60764c2eb0df44bf45a3c7f5ed
Ivan Arias <iarias@bilbomatica.es>
2014-11-26 11:40:38 +0100

    Blog Accordion

--- docroot/sites/all/modules/osha/osha_blog/includes/js/collapse_block.js
+++ docroot/sites/all/modules/osha/osha_blog/includes/js/collapse_block.js
@@ -3,12 +3,15 @@ jQuery(document).ready(function()
     // init: collapse all groups except for the first one
     jQuery(".blog-archive-block ul").each(function(i)
     {
-        jQuery(this).hide();
+        if(i != 0){
+            jQuery(this).hide();
+        }
     });
 
     // click event: toggle visibility of group clicked (and update icon)
     jQuery(".blog-archive-block h3").click(function()
     {
         jQuery(this).siblings("ul").slideToggle();
+		jQuery(this).toggleClass("expand");
     });
 });
